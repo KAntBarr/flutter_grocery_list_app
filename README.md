d41477823412be04ede91a9380c36d29f32d34e8
Koby <kobysac@gmail.com>
2024-03-14 23:12:34 -0700

    added comments and updated readme

--- README.md
+++ README.md
@@ -1,16 +1,9 @@
 # flutter_grocery_list_app
 
-A new Flutter project.
+## Description
+This application allows the user to save grocery items to a list. Grocery items can also be edited and removed. The grocery list can persist across sessions with the help of Share Preferences.
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## Resources
+- https://bloclibrary.dev/flutter-bloc-concepts/#blocprovider 
+- https://www.freecodecamp.org/news/learn-state-management-in-flutter/
+- https://www.youtube.com/watch?v=K4P5DZ9TRns
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-// import 'package:bloc/bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
@@ -19,13 +18,12 @@ class GroceryApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      debugShowCheckedModeBanner: false,
-      // home: const GroceryList(title: 'Grocery List'),
-      home: BlocProvider(
-        lazy: false,
+      debugShowCheckedModeBanner: false, // get rid of the debug banner
+      home: BlocProvider( // provide the overall bloc
+        lazy: false, // create bloc now
         create: (_) => ListBloc(),
         child: GroceryList(
-          title: 'Grocery List',
+          title: 'Grocery List', // title of the app
         ),
       ),
     );
@@ -37,19 +35,21 @@ class GroceryList extends StatelessWidget {
 
   final String title;
 
+  // text controller for the popup dialog to add items
   final TextEditingController _textFieldController = TextEditingController();
 
+  // like a promise, it will return a widget of a dialog
   Future<void> _displayDialog(context) async {
     return showDialog<void>(
       context: context,
-      barrierDismissible: false,
-      builder: (BuildContext contextS) {
+      barrierDismissible: false, // tapping outside the dialog won't do anything
+      builder: (BuildContext contextS) { // renamed to allow the outer context to have domain
         return AlertDialog(
           title: const Text('Add a item'),
           content: TextField(
             controller: _textFieldController,
             decoration: const InputDecoration(hintText: 'Type your item'),
-            autofocus: true,
+            autofocus: true, // the text field is selected
           ),
           actions: <Widget>[
             OutlinedButton(
@@ -59,7 +59,8 @@ class GroceryList extends StatelessWidget {
                 ),
               ),
               onPressed: () {
-                Navigator.of(context).pop();
+                Navigator.of(context).pop(); // close dialog
+                _textFieldController.clear(); // clear text field in dialog
               },
               child: const Text('Cancel'),
             ),
@@ -70,13 +71,10 @@ class GroceryList extends StatelessWidget {
                 ),
               ),
               onPressed: () {
-                Navigator.of(context).pop();
-                // context
-                //     .read<ListBloc>()
-                //     .add(AddEvent(_textFieldController.text));
-                BlocProvider.of<ListBloc>(context)
-                    .add(AddEvent(_textFieldController.text));
-                _textFieldController.clear();
+                Navigator.of(context).pop(); // close dialog
+                BlocProvider.of<ListBloc>(context) // trigger the event in the bloc
+                    .add(AddEvent(_textFieldController.text)); // with the text field content
+                _textFieldController.clear(); // clear text field in dialog
               },
               child: const Text('Add'),
             ),
@@ -88,28 +86,36 @@ class GroceryList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold( // the first layer/component/widget of the app
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(title),
       ),
+      // the body widget will react to changes 
+      // in the state because of the BlocBuilder
+      // the state being a list of items
       body: BlocBuilder<ListBloc, List<Item>>(
-        builder: (context, items) {
-          // List<Item> list = items as List<Item>;
-          return ListView(
+        builder: (context, items) { // provide the context/bloc and the state which is items
+          return ListView( // the widget that will hold the list items
             padding: const EdgeInsets.symmetric(vertical: 8.0),
-            children: items.map((Item item) {
+            children: items.map((Item item) { // map each item into a ListItem widget
               return ListItem(
                 item: item,
+                // attach a callback function(s) 
+                // to the ListItem that will
+                // trigger an event in the bloc
                 onItemChanged: (item, newName) =>
                     context.read<ListBloc>().add(EditEvent(item, newName)),
                 removeItem: (id) =>
                     context.read<ListBloc>().add(RemoveEvent(id)),
               );
-            }).toList(),
+            }).toList(), // turn into a list of ListItems
           );
         },
       ),
+      // the FOB also uses BlocBuilder
+      // to provide a context to the
+      // dialog in order to trigger events
       floatingActionButton: BlocBuilder<ListBloc, List<Item>>(
         builder: (context, items) {
           return FloatingActionButton(
@@ -123,6 +129,7 @@ class GroceryList extends StatelessWidget {
   }
 }
 
+// basic item class
 class Item {
   Item({required this.name, required this.id});
   // Item({required this.name, required this.completed});
@@ -131,6 +138,7 @@ class Item {
   String id;
 }
 
+// a class to view/react with the Item in a list on the screen
 class ListItem extends StatelessWidget {
   ListItem({
     required this.item,
@@ -140,12 +148,19 @@ class ListItem extends StatelessWidget {
 
   final Item item;
 
+  // wrapper function that triggers a bloc event
   final void Function(Item item, String newName) onItemChanged;
 
+  // wrapper function that triggers a bloc event
   final void Function(String id) removeItem;
 
+  // a text controller to control
+  // when a user clicks on an item
+  // to edit and save it
   final TextEditingController _textFieldController = TextEditingController();
 
+  // this function will return a
+  // text style but can return null
   TextStyle? _getTextStyle() {
     // TextStyle? _getTextStyle(bool checked) {
     // if (!checked) return null;
@@ -188,7 +203,9 @@ class ListItem extends StatelessWidget {
               ),
               onPressed: () {
                 Navigator.of(context).pop();
+                // if the text field is not empty, then edit item
                 if (_textFieldController.text != '') {
+                  // use the wrapper function attached to the object
                   onItemChanged(item, _textFieldController.text);
                 }
                 _textFieldController.clear();
@@ -210,13 +227,13 @@ class ListItem extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.only(left: 30.0),
             child: GestureDetector(
-                child: Text(
+                child: Text( // text widget that displays our item name
               item.name,
               style: _getTextStyle(),
             )),
           ),
         ),
-        IconButton(
+        IconButton( // delete button
           iconSize: 30,
           icon: const Icon(
             Icons.delete,
@@ -224,7 +241,7 @@ class ListItem extends StatelessWidget {
           ),
           alignment: Alignment.centerRight,
           onPressed: () {
-            removeItem(item.id);
+            removeItem(item.id); // wrapper function to delete item
           },
         ),
       ]),
@@ -232,14 +249,18 @@ class ListItem extends StatelessWidget {
   }
 }
 
+// set up for using events
+// the base event type
 abstract class ItemEvent {}
 
+// add event to create an item
 class AddEvent extends ItemEvent {
   final String name;
 
   AddEvent(this.name);
 }
 
+// edit event to edit an item's name
 class EditEvent extends ItemEvent {
   final Item item;
   final String newName;
@@ -247,60 +268,81 @@ class EditEvent extends ItemEvent {
   EditEvent(this.item, this.newName);
 }
 
+// remove event to delete an item
 class RemoveEvent extends ItemEvent {
   final String id;
 
   RemoveEvent(this.id);
 }
 
+// custom bloc class
 class ListBloc extends Bloc<ItemEvent, List<Item>> {
   ListBloc() : super(<Item>[]) {
-    _loadList();
+    _loadList(); // initialize list state on creation
 
     on<AddEvent>((event, emit) async {
-      final prefs = await SharedPreferences.getInstance();
-      String time = DateTime.now().millisecondsSinceEpoch.toString();
-      List<String> itemIds = state.map((Item item) {
-        return item.id;
-      }).toList();
-      itemIds.add(time);
-      await prefs.setStringList('itemIds', itemIds);
-      await prefs.setString(time, event.name);
-      List<Item> temp = state.toList();
-      temp.add(Item(
-        name: event.name,
-        id: time,
-      ));
-      emit(temp);
+      try {
+        final prefs = await SharedPreferences.getInstance(); // get link to storage
+        String time = DateTime.now().millisecondsSinceEpoch.toString(); // get time for id
+        List<String> itemIds = state.map((Item item) { // get current list of item ids
+          return item.id;
+        }).toList();
+        // add time to list of item ids
+        itemIds.add(time);
+        await prefs.setStringList('itemIds', itemIds); // set this new list in storage
+        // using the time/id, save
+        // the given name to storage
+        await prefs.setString(time, event.name); 
+        List<Item> temp = state.toList(); // get a copy of the state(list of items)
+        temp.add(Item( // add a new item
+          name: event.name,
+          id: time,
+        ));
+        emit(temp); // have the bloc builders react to the state change
+      } catch (error) {
+        print('Error adding data: $error');
+      }
     });
 
     on<EditEvent>((event, emit) async {
-      if (event.newName != '') {
-        final prefs = await SharedPreferences.getInstance();
-        await prefs.setString(event.item.id, event.newName);
-        event.item.name = event.newName;
-        emit(state.toList());
+      try {
+        // if the given new name is not empty
+        // then save the edit to storage 
+        if (event.newName != '') {
+          final prefs = await SharedPreferences.getInstance(); // get link to stored data
+          // set the new name to storage using the id as a key
+          await prefs.setString(event.item.id, event.newName);
+          event.item.name = event.newName;
+          emit(state.toList()); // emit so bloc builders can update
+        }
+      } catch (error) {
+        print('Error editing data: $error');
       }
     });
 
     on<RemoveEvent>((event, emit) async {
-      final prefs = await SharedPreferences.getInstance();
-      List<Item> items =
-          state.where((element) => element.id != event.id).toList();
-      items.removeWhere((element) => element.id == event.id);
-      List<String> itemIds = items.map((Item item) {
-        return item.id;
-      }).toList();
-      await prefs.setStringList('itemIds', itemIds);
-      await prefs.remove(event.id);
-      emit(items);
+      try {
+        final prefs = await SharedPreferences.getInstance(); // get link to stored data
+        List<Item> items = // filter the list to not include the item with the given id
+            state.where((element) => element.id != event.id).toList();
+        List<String> itemIds = items.map((Item item) { // get list of item ids
+          return item.id;
+        }).toList();
+        await prefs.setStringList('itemIds', itemIds); // save the new list to storage
+        await prefs.remove(event.id); // remove the old item from storage with the given id
+        emit(items); // set the state to the new list of items
+      } catch (error) {
+        print('Error removing data: $error');
+      }
     });
   }
 
+  // initialize the state list
   Future<void> _loadList() async {
     try {
-      final prefs = await SharedPreferences.getInstance();
-      final List<String>? itemIds = prefs.getStringList('itemIds');
+      final prefs = await SharedPreferences.getInstance(); // get link to stored data
+      final List<String>? itemIds = prefs.getStringList('itemIds'); // get item IDs
+      // map through the IDs to build a list of Item widgets
       final List<Item> completeItems = itemIds?.map((String id) {
             return Item(
               name: prefs.getString(id) ?? 'error',
@@ -309,10 +351,10 @@ class ListBloc extends Bloc<ItemEvent, List<Item>> {
           }).toList() ??
           [];
       emit(
-          completeItems); // Emit the initial state with data from SharedPreferences
-    } catch (e) {
+          completeItems); // Emit the initial state with data from storage
+    } catch (error) {
       // Handle errors, if any
-      print('Error initializing data: $e');
+      print('Error initializing data: $error');
     }
   }
 }
